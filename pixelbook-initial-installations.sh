cea696278e1c4929e94d0089f2399ff096622dfe
Amit Wolfenfeld <wolfenfeld@gmail.com>
2019-10-08 10:47:35 +0300

    initial commit

---
+++ pixelbook-initial-installations.sh
@@ -0,0 +1,26 @@
+
+# zsh, oh my zsh
+
+# Conda
+wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+.Anaconda3-2019.07-Linux-x86_64.sh
+
+# Chromium
+apt-get install chromium
+
+# Jupyter Lab
+
+# Git Kraken
+apt-get install libgnome-keyring0
+apt-get install libcurl3
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+dpkg -i gitkraken-amd64.deb
+
+# Atom
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+
+# Pycharm
+pycharm
+
