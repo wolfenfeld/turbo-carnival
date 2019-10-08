7832c7b61823f19a802d48e3f9fa1f2d4af14dbc
Amit Wolfenfeld <wolfenfeld@gmail.com>
2019-10-08 11:53:23 +0300

    Update pixelbook-initial-installations.sh
    updated zsh and anaconda

--- pixelbook-initial-installations.sh
+++ pixelbook-initial-installations.sh
@@ -1,8 +1,19 @@
+mkdir Applications
+
+# update and upgrade
+sudo apt-get update -y
+sudo apt upgrade -y
 
 # zsh, oh my zsh
+sudo apt install zsh -y
+sudo apt-get install powerline fonts-powerline -y
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sudo chsh -s /usr/bin/zsh $USER
 
 # Conda
 wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+sudo chmod 777 Anaconda3-2019.07-Linux-x86_64.sh
 .Anaconda3-2019.07-Linux-x86_64.sh
 
 # Chromium
